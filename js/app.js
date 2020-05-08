956b8065f7b4bfaebfb1e85c34a7d7951e4a945b
Muhammad Kashif <mkashif.aw@gmail.com>
2020-05-08 23:46:45 +0500

    Add images and other css and js files

---
+++ js/app.js
@@ -0,0 +1,38 @@
+{/* <div class="container">
+        <div class="card" style="width: 18rem;">
+            <img class="" src="images/onlinesadqah.png" alt="Card image cap" width="75px">
+            <div class="card-body">
+                <p class="card-text">ONLINE SADQAH</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="images/welfare.png" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-text">WELFARE</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="images/medical.png" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-text">MEDICAL</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="images/Dastarkhwan.png" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-text">DASTARKHWAN</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="images/ROPLANT.png" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-text">RO PLANT</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="images/education.png" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-text">EDUCATION</p>
+            </div>
+        </div>
+    </div> */}
